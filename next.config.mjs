b7f5f2463e6bb63e3cc6a4e430af828e816e87c4
Asad khan <asadkhan29511@gmail.com>
2026-01-14 18:43:03 +0500

    ......

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      type: "javascript/auto"
+    });
+
+    if (!isServer) {
+      config.resolve.mainFields = ["browser", "module", "main"];
+    }
+
+    return config;
+  }
 };
 export default nextConfig;
